9859a5b2f9d396281d1923ab2c2440417b62922c
xjjx <xj@wp.pl>
2012-10-26 10:09:56 +0000

    Cleanup code
    git-svn-id: svn+ssh://bigpig/bigpig/svn/fhctrl@6 f43a4bc2-07ea-415d-9173-feacbafa7299

--- nfhc.c
+++ nfhc.c
@@ -1,4 +1,4 @@
-//      xj-interface.c
+//      nfhc.c
 //      
 //      Shamefully done by blj <blindluke@gmail.com>
 //      
@@ -24,11 +24,11 @@ struct labelbox {
     uint8_t fstid;
 };
 
-CDKSWINDOW *logwin;
+CDKSWINDOW *logwin = NULL;
 CDKSCROLL *song_list;
 bool quit = false;
-extern void send_ident_request();
-extern void update_lcd();
+extern void send_ident_request(); // From sysex.c
+extern void update_lcd();         // From fhctrl.c
 int state_color[3] = { 58, 59, 57 };
 
 static int get_selector_1(CDKSCREEN *cdkscreen) {
@@ -80,23 +80,45 @@ static int get_selector_1(CDKSCREEN *cdkscreen) {
     return (choice + 1);
 }
 
+void update_selector(struct labelbox *selector, struct FSTPlug *fp) {
+   char text[2][LABEL_LENGHT];
+   char* ptext[2] = { text[0], text[1] };
+
+   snprintf(text[0], LABEL_LENGHT,"</U/%d>%03d %-23s | %02d | %02d<!05>",
+      state_color[fp->state->state], 
+      fp->id,
+      fp->name,
+      fp->state->channel,
+      fp->state->volume
+   );
+   snprintf(text[1], LABEL_LENGHT,"#%02d - %-24s", fp->state->program, fp->state->program_name);
+   setCDKLabelMessage(selector->label, ptext, 2);
+}
+
 void nLOG(char *fmt, ...) {
    char info[LOGWIN_WIDTH];
    va_list args;
 
    va_start(args, fmt);
-   vsnprintf(info, sizeof(info), fmt, args);
-   addCDKSwindow(logwin, info, 0);
+
+  vsnprintf(info, sizeof(info), fmt, args);
+   if (logwin != NULL) {
+      addCDKSwindow(logwin, info, 0);
+   } else {
+      printf("%s\n", info);
+   }
+
+   va_end(args);
 }
 
 void nfhc(struct Song *song_first, struct FSTPlug **fst) {
-    short i, f, j;
-    bool lcd_update = false;
+    short i, j;
+    int lm = 0, tm = 0;
+    bool lcd_need_update = false;
     CDKSCREEN       *cdkscreen;
     CDKLABEL        *top_logo;
     WINDOW          *screen;
     char            *mesg[9];
-    char            *text[2];
     struct labelbox selector[16];
     struct FSTPlug *fp;
     struct Song *song;
@@ -135,59 +157,56 @@ void nfhc(struct Song *song_first, struct FSTPlug **fst) {
     drawCDKScroll(song_list, TRUE);
 
     /* SELECTOR init - same shit for all boxes */
-    for(i=0; i<2; i++) text[i] = malloc( sizeof(char) * LABEL_LENGHT );
+    {
+      char text[2][LABEL_LENGHT];
+      char* ptext[2] = { text[0], text[1] };
 
-    int lm = 0, tm = 0;
-    for (i  = 0; i < 16; i++, tm += 4) {
-        if (i == 8) {
-           lm = CHUJ+2;
-           tm = 0;
-	}
+      for(i=0; i < 2; i++) cleanChar(text[i], CHUJ, '-');
+      for (i  = 0; i < 16; i++, tm += 4) {
+         if (i == 8) {
+            lm = CHUJ+2;
+            tm = 0;
+	 }
 
-	for(j=0; j < 2; j++) cleanChar(text[j], CHUJ, '-');
-
-        selector[i].label = newCDKLabel (cdkscreen, LEFT_MARGIN+lm, TOP_MARGIN+(tm), text, 2, TRUE, FALSE);
-        drawCDKLabel (selector[i].label, TRUE);
+         selector[i].label = newCDKLabel (cdkscreen, LEFT_MARGIN+lm, TOP_MARGIN+(tm), ptext, 2, TRUE, FALSE);
+         drawCDKLabel (selector[i].label, TRUE);
+      }
     }
 
-    int k;
     timeout(300);
     cbreak();
     noecho();
     while(! quit) {
        // For our boxes
-       for (i = f = 0; i < 16; i++) {
+       for (i = j = 0; i < 16; i++) {
           // Get next FST
-          while (f < 128) {
-             fp = fst[f++];
+          while (j < 128) {
+             fp = fst[j++];
              if (!fp) continue;
              if (selector[i].fstid == fp->id && !fp->change)
                 break;
 
              fp->change = false;
-             lcd_update = true;
-
-             snprintf(text[0], LABEL_LENGHT,"</U/%d>%03d %-23s | %02d | %02d<!05>",
-                state_color[fp->state->state], 
-                fp->id,
-                fp->name,
-                fp->state->channel,
-                fp->state->volume
-             );
-             snprintf(text[1], LABEL_LENGHT,"#%02d - %-24s", fp->state->program, fp->state->program_name);
-
-             setCDKLabelMessage(selector[i].label, text, 2);
+             lcd_need_update = true;
+             update_selector(&selector[i], fp);
 
 	     break;
           }
        }
-       if (lcd_update) {
-          lcd_update = false;
+
+       if (lcd_need_update) {
+          lcd_need_update = false;
           update_lcd();
        }
-       //usleep(300000);
-       k = getch();
-       if(k == 'q') quit = true;
+
+       i = getch();
+       switch(i) {
+         case 'q':
+             quit = true;
+             break;
+//         case 's':
+             
+       }
        refreshCDKScreen(cdkscreen);
     }
 
@@ -197,6 +216,7 @@ void nfhc(struct Song *song_first, struct FSTPlug **fst) {
     destroyCDKScreen(cdkscreen);
     destroyCDKLabel(top_logo);
     destroyCDKSwindow(logwin);
+    logwin = NULL;
     destroyCDKScroll(song_list);
     for (i = 0; i < 16; i++) destroyCDKLabel(selector[i].label);
 
