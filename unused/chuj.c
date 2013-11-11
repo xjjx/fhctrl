92e2525261ed7b1f528d957f8a3f0326b326e021
xjjx <xj@wp.pl>
2013-11-11 01:23:21 +0000

    Chuj - resotre connections, clean quit, graph changed etc.
    git-svn-id: svn+ssh://bigpig/bigpig/svn/fhctrl@148 f43a4bc2-07ea-415d-9173-feacbafa7299

--- unused/chuj.c
+++ unused/chuj.c
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 
 #include <jack/jack.h>
+#include <jack/session.h>
 
 #include <cdk.h>
 
@@ -22,7 +23,12 @@ typedef struct {
         char *dst;
 } connection_t;
 
-int child ( char* cmd ) {
+static volatile bool quit = false;
+void signal_handler (int sig) { quit = true; }
+
+void jack_shutdown (void *arg) { quit = true; }
+
+int run_cmd ( char* cmd ) {
 	execlp ( "sh", "sh", "-c", cmd, NULL );
 	return 1;
 }
@@ -56,56 +62,118 @@ void app_wait ( app_t* app ) {
 	}
 }
 
-bool check_con ( jack_client_t* client, connection_t* con ) {
+int uuid2name ( jack_client_t *client, const char* arg, char* outbuf, size_t outbuf_size ) {
+	char *port_part = strchr( arg, ':' );
+	size_t size = port_part - arg + 1;
+	char uuid[size];
+	snprintf ( uuid, size, "%s", arg );
+
+	char *clientname = jack_get_client_name_by_uuid ( client, uuid );
+	if ( ! clientname ) return 0;
+
+	if ( outbuf_size < strlen(clientname) + strlen(port_part) + 1)
+		return 0;
+
+	snprintf ( outbuf, outbuf_size, "%s%s", clientname, port_part );
+	jack_free (clientname);
+
+	return 1;
+}
+
+jack_port_t* get_port ( jack_client_t* client, char* name, int flags ) {
 	int port_name_size = jack_port_name_size();
 	char portName[port_name_size];
-/*
-	for (n=argc-2; n < argc; n++) {
-		if (! use_uuid || ! uuid2name(client, portName, argv[n], sizeof portName ) ) {
-			snprintf( portName, sizeof(portName), "%s", argv[n] );
-		}
-*/
 
-	snprintf( portName, sizeof portName, "%s", con->src );
-	jack_port_t* sport = jack_port_by_name (client, portName);
+	if ( ! uuid2name ( client, name, portName, sizeof portName ) )
+		strncpy ( portName, name, sizeof(portName) - 1 );
+
+	jack_port_t* port = jack_port_by_name ( client, portName );
+	if ( ! port ) return NULL;
+
+	int port_flags = jack_port_flags (port);
+	if ( ! ( port_flags & flags) ) return NULL;
+
+	return port;
+}
+
+const char* get_port_name ( jack_client_t* client, char* name, int flags ) {
+	jack_port_t* port = get_port ( client, name, flags );
+	if ( ! port ) return NULL; 
+	return jack_port_name ( port );
+}
+
+bool check_con ( connection_t* con, jack_client_t* client ) {
+	/* Get src port */
+	jack_port_t* sport = get_port ( client, con->src, JackPortIsOutput );
 	if ( ! sport ) return false;
 
-	int port_flags = jack_port_flags (sport);
-	if ( ! ( port_flags & JackPortIsOutput) ) return false;
-
-	// Update uuid2name mapping if needed
-//	if (use_uuid) uuid2name(client, portName, argv[n], sizeof(portName));
-//	}
-	
-	snprintf( portName, sizeof portName, "%s", con->dst );
-	jack_port_t* dport = jack_port_by_name (client, portName);
+	/* Get dst port */
+	jack_port_t* dport = get_port ( client, con->dst, JackPortIsInput );
 	if ( ! dport ) return false;
+	const char* dport_name = jack_port_name ( dport );
 
-	port_flags = jack_port_flags (dport);
-	if ( ! ( port_flags & JackPortIsInput) ) return false;
-
+	/* Check connections */
 	const char** conn = jack_port_get_all_connections ( client, sport );
 	if (! conn) return false;
 	
 	int i;
-	bool ret = false;
-	for (i=0; conn[i]; i++) {
-		if ( !strcmp ( conn[i], con->dst ) ) {
-			ret = true;
-			break;
+	for ( i=0; conn[i]; i++ ) {
+		if ( !strcmp ( conn[i], dport_name ) ) {
+			jack_free (conn);
+			return true;
 		}
 	}
 	jack_free (conn);
-	return ret;
+	return false;
+}
+
+void refresh_conlist ( CDKSCROLL* conlist, connection_t* con, int num, jack_client_t* client ) {
+	setCDKScrollItems ( conlist, NULL, 0, 0 );
+	int i;
+	for ( i = 0; i < num; i++ ) {
+		bool connected = check_con ( &con[i], client );
+		char chuj[256];
+		snprintf ( chuj, sizeof chuj, "%s -> %s (%s)", con[i].src, con[i].dst, (connected)?"CONNECTED":"WAIT" );
+		addCDKScrollItem ( conlist, chuj );
+	}
+	drawCDKScroll ( conlist, TRUE );
+}
+
+int graph_cb_handler ( void *arg ) {
+	bool* graph_changed = (bool*) arg;
+	*graph_changed = true;
+
+        return 0;
+}
+
+void restore_connections ( connection_t* con, int num, jack_client_t* client ) {
+	int i;
+	for ( i = 0; i < num; i++ ) {
+		bool connected = check_con ( &con[i], client );
+		if ( connected ) continue;
+
+		/* Can not pass name directly because of uuid mapping */
+		const char* src = get_port_name ( client, con->src, JackPortIsOutput );
+		if ( ! src ) continue;
+
+		const char* dst = get_port_name ( client, con->dst, JackPortIsInput );
+		if ( ! dst ) continue;
+
+		jack_connect (client, src, dst);
+	}
 }
 
 int main (int argc, char *argv[]) {
+	bool graph_changed = true;
+
 	/* init jack */
 	jack_client_t* client = jack_client_open ( "CHUJ" , JackNullOption, NULL);
 	if ( client == 0 ) {
 		fprintf(stderr, "JACK server not running?\n");
 		return 1;
 	}
+        jack_set_graph_order_callback (client, graph_cb_handler, &graph_changed);
+	jack_on_shutdown ( client, jack_shutdown, 0 );
 	jack_activate ( client );
 
 	/* Initialize the Cdk screen.  */
@@ -133,14 +201,11 @@ int main (int argc, char *argv[]) {
 		"</U/63>CONNECTIONS list:<!05>", 0, 0, FALSE, A_NORMAL, TRUE, FALSE
 	);
 
-/*
 	signal(SIGQUIT, signal_handler);
 	signal(SIGTERM, signal_handler);
 	signal(SIGHUP, signal_handler);
 	signal(SIGINT, signal_handler);
 
-	jack_on_shutdown ( client, jack_shutdown, 0 );
-*/
 
 	/* Init APP */
 	int i;
@@ -156,12 +221,12 @@ int main (int argc, char *argv[]) {
 		con[i].dst = "system:playback_1";
 	}
 
-	while ( true ) {
+	while ( ! quit ) {
 		for ( i = 0; i < CH; i++ ) {
 			if ( app[i].ready ) {
 				pid_t p = fork();
 				if ( p == 0 ) { // Child
-					return child ( app[i].cmd );
+					return run_cmd ( app[i].cmd );
 				} else if ( p < 0 ) { // FORK FAIL // Parent
 					app[i].pid = 0;
 				} else { // p > 0 // Parent
@@ -173,18 +238,15 @@ int main (int argc, char *argv[]) {
 			app_wait ( &app[i] );
 		}
 		refresh_applist ( applist, app, CH );
-
-		setCDKScrollItems ( conlist, NULL, 0, 0 );
-		for ( i = 0; i < CC; i++ ) {
-			bool connected = check_con ( client, &con[i] );
-			char chuj[256];
-			snprintf ( chuj, sizeof chuj, "%s -> %s (%s)", con[i].src, con[i].dst, (connected)?"CONNECTED":"WAIT" );
-			addCDKScrollItem ( conlist, chuj );
+		if ( graph_changed ) {
+			restore_connections ( con, CC, client );
+			refresh_conlist ( conlist, con, CC, client );
 		}
-		drawCDKScroll ( conlist, TRUE );
+
 		sleep ( 1 );
 	}
 
+	/* Cleanup */
 	destroyCDKScroll ( applist );
 	destroyCDKScroll ( conlist );
 	destroyCDKScreen ( cdkscreen );
@@ -192,6 +254,8 @@ int main (int argc, char *argv[]) {
 
         jack_deactivate ( client );
         jack_client_close ( client );
+
+	puts ( "This was clean .." );
 
 	return 0;
 }
